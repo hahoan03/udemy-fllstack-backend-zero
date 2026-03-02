3858e9ccfa5c18189759a441e8cc93b49e88ce42
Ha Xuan Hoan <hoanha0511abc@gmail.com>
2026-03-02 18:43:11 +0700

    abc

---
+++ src/server.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const path = require('path');
+require('dotenv').config();
+
+console.log(">> check env:", process.env);
+
+const app = express()
+const port = process.env.PORT || 3000;
+const hostname = process.env.HOST_NAME;
+
+
+//config template engine
+app.set('views', path.join(__dirname, 'view'))
+app.set('view engine', 'ejs')
+
+//Khai bao route
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/abc', (req, res) => {
+    res.send('check abc!')
+})
+
+app.get('/hoidanit', (req, res) => {
+    // res.send('<h1>hoi dan it with hahoan</h1>')
+    res.render('sample.ejs')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`Example app listening on port ${port}`)
+})
