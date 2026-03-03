ea270d27af5cc50086a38a5dcc89088fad7d1c1d
Ha Xuan Hoan <hoanha0511abc@gmail.com>
2026-03-03 20:58:59 +0700

    abc

--- src/server.js
+++ src/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
-const express = require('express')
+const express = require('express');
 const path = require('path');
-const configViewEngine = require('./config/viewEngine')
-const webRoutes = require('./routes/web')
+const configViewEngine = require('./config/viewEngine');
+const webRoutes = require('./routes/web');
+const connection = require('./config/database');
+
 
 const app = express()
 const port = process.env.PORT || 3000;
@@ -15,6 +17,18 @@ configViewEngine(app);
 
 //Khai bao route
 app.use('/test', webRoutes);
+
+//test connection
+
+
+// A simple SELECT query
+connection.query(
+    'select * from Users u',
+    function (err, results, fields) {
+        console.log('>>>results=', results); // results contains rows returned by server
+    }
+);
+
 
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
