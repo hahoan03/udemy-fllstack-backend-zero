cacc6b59616dc7729f6d9fb830c09497f0bfbdbb
Ha Xuan Hoan <hoanha0511abc@gmail.com>
2026-03-04 21:44:13 +0700

    abc

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,10 +1,10 @@
 const { json } = require('express');
 const connection = require('../config/database')
+const { getAllUsers } = require('../services/CRUDService')
 
-
-const getHomepage = (req, res) => {
-
-    return res.render('home.ejs')
+const getHomepage = async (req, res) => {
+    let results = await getAllUsers();
+    return res.render('home.ejs', { listUsers: results })
 }
 
 const getABC = (req, res) => {
@@ -15,7 +15,7 @@ const getHoiDanIT = (req, res) => {
     res.render('sample.ejs')
 }
 
-const postCreateUser = (req, res) => {
+const postCreateUser = async (req, res) => {
 
     let email = req.body.email;
     let name = req.body.myname;
@@ -23,19 +23,25 @@ const postCreateUser = (req, res) => {
     //hoặc viết let {email, name, city} = req.body thay cho 3 dòng trên
     console.log(">>> email: ", email, " name= ", name, ' city= ', city);
 
-    connection.query(
+    let [results, fields] = await connection.query(
         `INSERT INTO   
             Users(email, name, city)
             VALUES(?, ?, ?)`,
-        [email, name, city],
-        function (err, results) {
-            console.log(results);
+        [email, name, city]
+    );
 
-            res.send('Create user succeed!');
-        }
-    )
+    //console.log(">>>> check results: ", results)
+    res.send("create user succeed")
+
 }
 
+
+const getCreatePage = (req, res) => {
+    res.render('create.ejs');
+}
+
+
 module.exports = {
-    getHomepage, getABC, getHoiDanIT, postCreateUser
+    getHomepage, getABC, getHoiDanIT, postCreateUser,
+    getCreatePage
 }
